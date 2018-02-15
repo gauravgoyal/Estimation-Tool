dee5d8fb0daa946e95fc771766ade4f1c9989fa0
Abhishek Anand <fly2abhishek@gmail.com>
2018-02-15 16:45:58 +0530

    Adding core UI for react.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Estimation API',
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3001,
-  base_url: process.env.BASE_URL || 'http://localhost:3000',
+  base_url: process.env.BASE_URL || 'http://localhost:3001',
   db: {
     uri: process.env.MYSQL_URI || 'mysql://root@127.0.0.1/estimation',
   },
