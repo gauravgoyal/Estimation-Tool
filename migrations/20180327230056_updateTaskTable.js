a6bb7e0545973dfaabb30fc9f902636a9a71c467
Gaurav Goyal <gaurav.goyal@acquia.com>
2018-03-30 13:02:39 +0530

    Schema updated.

--- migrations/20180327230056_updateTaskTable.js
+++ migrations/20180327230056_updateTaskTable.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('tasks', function(table) {
       table.dropColumn('confidence');
-      table.integer('ufid').unsigned().notNullable().references('pid').inTable('projects').onDelete('CASCADE').index();
+      table.integer('ufid').unsigned().notNullable().references('ufid').inTable('uncertainity_factors').onDelete('CASCADE').index();
       table.integer('estimated_hours');
       table.integer('rate_low');
       table.integer('rate_high');
